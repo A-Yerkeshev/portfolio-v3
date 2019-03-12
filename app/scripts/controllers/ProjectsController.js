b17b2062d44ed07554a4968307e2d5b953eb24e4
Arman Yerkesh <a.yerkesh@gmail.com>
2019-03-12 12:38:19 +0300

    Display projects titles on hover

--- app/scripts/controllers/ProjectsController.js
+++ app/scripts/controllers/ProjectsController.js
@@ -1,3 +1,11 @@
 Portfolio.controller('ProjectsController', function($scope, ProjectsFactory) {
   $scope.projects = ProjectsFactory.getAllProjects();
+
+  $scope.showTitle = function(element) {
+    element.style.opacity = 1;
+  };
+
+  $scope.hideTitle = function(element) {
+    element.style.opacity = 0;
+  };
 });
