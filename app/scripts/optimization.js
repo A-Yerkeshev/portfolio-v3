62ef42775c6a01e77dc53e2fb7a7e80abb1e5e62
Arman Yerkesh <a.yerkesh@gmail.com>
2021-05-12 10:30:59 +0300

    Animate welcoming text

--- app/scripts/optimization.js
+++ app/scripts/optimization.js
@@ -2,11 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Animate welcome heading
   let heading = document.querySelectorAll('.w-char');
+  let par = document.querySelector('.w-par');
 
   heading.forEach((char, i) => {
-    // Move each characted from the bottom
+    // Move each character from the bottom
     setTimeout(() => {
       char.classList.remove('hidden');
     }, i*200)
   })
+
+  // Appear welcome paragraph
+  setTimeout(() => {
+    par.classList.remove('hidden');
+  }, 2200)
 })
