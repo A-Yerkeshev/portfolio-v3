ea2bc81f104614a88d5cf590540b2fda68663c4c
Arman Yerkesh <a.yerkesh@gmail.com>
2021-05-13 11:37:03 +0300

    Animate welcome heading and paragraph without JavaScript

--- app/scripts/optimization.js
+++ app/scripts/optimization.js
@@ -1,18 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  // Animate welcome heading
-  let heading = document.querySelectorAll('.w-char');
-  let par = document.querySelector('.w-par');
-
-  heading.forEach((char, i) => {
-    // Move each character from the bottom
-    setTimeout(() => {
-      char.classList.remove('hidden');
-    }, i*200)
-  })
-
-  // Appear welcome paragraph
-  setTimeout(() => {
-    par.classList.remove('hidden');
-  }, 2200)
+  document.querySelector('#continue-btn').style.opacity=1;
 })
