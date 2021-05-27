c7fedbeccc00b67cd15f4d3d9ab4581a82b85631
Arman Yerkesh <a.yerkesh@gmail.com>
2021-05-27 11:04:43 +0300

    Replace spinner with continue button on page load

--- app/scripts/onload.js
+++ app/scripts/onload.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#continue-btn').style.opacity=1;
+  let spinner = document.querySelector('.spinner');
+  let continueBtn = document.querySelector('#continue-btn');
+
+  spinner.style.opacity = 0;
+  setTimeout(() => {
+    spinner.style.display = 'none';
+    continueBtn.style.display = 'inline-block';
+  }, 1000)
+  setTimeout(() => {
+    continueBtn.style.opacity = 1;
+  }, 1200)
 })
 
