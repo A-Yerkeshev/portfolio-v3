b9d0026294de63fa929236f5f24f9407bd4720f4
Arman Yerkesh <a.yerkesh@gmail.com>
2021-07-31 15:01:57 +0300

    Minify images

--- app/imageCompressor.js
+++ app/imageCompressor.js
@@ -1,12 +1,12 @@
 const compressor = require("compress-images");
 
-// const inputPath = `./images/main/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}`;
-const inputPath = `./images/main/all-movies-1.png`;
+const inputPath = `./images/main/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}`;
+// const inputPath = `./images/main/all-movies-1.png`;
 const outputPath = './images/minified/';
 
 compressor(inputPath, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
                 { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
-                { png: { engine: "pngquant", command: ["--quality=10-20", "-o"] } },
+                { png: { engine: "pngquant", command: ["--quality=1-1", "-o"] } },
                 { svg: { engine: "svgo", command: "--multipass" } },
                 { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
   function (error, completed, statistic) {
