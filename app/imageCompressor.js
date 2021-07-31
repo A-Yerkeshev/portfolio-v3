2821a254d319b787cc48e9716f48a25df2d0e847
Arman Yerkesh <a.yerkesh@gmail.com>
2021-07-31 14:52:28 +0300

    Install image compressor images/main/dashboard-5.png

---
+++ app/imageCompressor.js
@@ -0,0 +1,19 @@
+const compressor = require("compress-images");
+
+// const inputPath = `./images/main/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}`;
+const inputPath = `./images/main/all-movies-1.png`;
+const outputPath = './images/minified/';
+
+compressor(inputPath, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+                { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+                { png: { engine: "pngquant", command: ["--quality=10-20", "-o"] } },
+                { svg: { engine: "svgo", command: "--multipass" } },
+                { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+  function (error, completed, statistic) {
+    console.log("-------------");
+    console.log(error);
+    console.log(completed);
+    console.log(statistic);
+    console.log("-------------");
+  }
+);
