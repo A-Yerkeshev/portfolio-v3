b0794f5c077dcf6c3a7f855cf33449e6b8423d42
Arman Yerkesh <a.yerkesh@gmail.com>
2021-08-17 08:16:37 +0300

    Remove rotation animation and bring icons up on welcoming page

--- Gruntfile.js
+++ Gruntfile.js
@@ -314,7 +314,7 @@ module.exports = function (grunt) {
     cssmin: {
       dist: {
         files: {
-          '<%= yeoman.dist %>/styles/main.css': [
+          '<%= yeoman.dist %>/styles/*.css': [
             '.tmp/styles/{,*/}*.css'
           ]
         }
